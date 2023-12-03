d40fd735c5ffbb09cebad30c5a3aa4056c6d57cb
Lucas Johns <johnslw26@gmail.com>
2023-12-02 20:35:23 -0500

    Modified the header to maybe look better, but again I don't like it

--- playground/src/Header.tsx
+++ playground/src/Header.tsx
@@ -1,52 +1,58 @@
 function Header() {
-	return (
-		<>
-			<header>
-				<h1>
-					Family Haven Hub
-					<span>A "family" company</span>
-				</h1>
-				<nav>
-					<ul>
-						<li>
-							<a href="#">Home</a>
-						</li>
-						<li>
-							<a href="#">Services</a>
-							<ul>
-								<li>
-									<a href="#">Web Design</a>
-								</li>
-								<li>
-									<a href="#">Graphic Design</a>
-								</li>
-								<li>
-									<a href="#">SEO</a>
-								</li>
-							</ul>
-						</li>
-						<li>
-							<a href="#">Portfolio</a>
-							<ul>
-								<li>
-									<a href="#">Project 1</a>
-								</li>
-								<li>
-									<a href="#">Project 2</a>
-								</li>
-								<li>
-									<a href="#">Project 3</a>
-								</li>
-							</ul>
-						</li>
-						<li>
-							<a href="#">Contact</a>
-						</li>
-					</ul>
-				</nav>
-			</header>
-		</>
-	);
+  return (
+    <>
+      <header>
+        <h1>
+          Family Haven Hub
+          <span>A "family" company</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+              <ul>
+                <li>
+                  <a href="#">Web Design</a>
+                </li>
+                <li>
+                  <a href="#">Graphic Design</a>
+                </li>
+                <li>
+                  <a href="#">SEO</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">Portfolio</a>
+              <ul>
+                <li>
+                  <a href="#">Project 1</a>
+                </li>
+                <li>
+                  <a href="#">Project 2</a>
+                </li>
+                <li>
+                  <a href="#">Project 3</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
 
 export default Header;
